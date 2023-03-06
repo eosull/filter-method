cf9ffd9ebe6143a76ecac182727967a72cc45356
eosull <osullivaneoin442@gmail.com>
2023-03-06 16:25:18 +0000

    Completed filter module

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const findPaul = people.filter(p => p.name === 'Paul')[0];
+console.log(findPaul);
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,10 @@ const students = [
     ]
   },
 ];
+const has5YearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5YearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
+
+const candidateNames = candidates.map(n => n.name);
+console.log(candidateNames);
